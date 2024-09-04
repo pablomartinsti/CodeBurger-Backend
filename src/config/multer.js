f40b8943f2906c9814c4f6e3c3e0c066f701dbf0
pablomartinsti <pablomartinsti@outlook.com>
2024-09-03 22:18:47 -0300

    atualizaçao do codigo

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,14 +1,12 @@
-import multer from 'multer'
-import { v4 } from 'uuid'
+import multer from 'multer';
+import { v4 } from 'uuid';
 
-import { extname, resolve } from 'node:path'
-
+import { extname, resolve } from 'node:path';
 
 export default {
-    storage: multer.diskStorage({
-        destination: resolve(__dirname, '..', '..', 'uploads'),
-        filename: (request, file, callback) =>
-            callback(null, v4() + extname(file.originalname))
-    })
-
-}
+  storage: multer.diskStorage({
+    destination: resolve(__dirname, '..', '..', 'uploads'),
+    filename: (request, file, callback) =>
+      callback(null, v4() + extname(file.originalname)),
+  }),
+};
--- src/database/index.js
+++ src/database/index.js
@@ -25,7 +25,7 @@ class Database {
   }
 
   mongo() {
-    this.mongoConnection = mongoose.connect('mongodb://localhost:27017/loja');
+    this.mongoConnection = mongoose.connect('mongodb://localhost:27017/');
   }
 }
 
